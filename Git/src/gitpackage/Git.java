461ff7ece33f14fcfcf5ec6a753923d6b6ea8f37
gloriamadhri <gloriamadhri@gmail.com>
2024-10-03 19:19:14 +0530

    updated2.0

--- Git/src/gitpackage/Git.java
+++ Git/src/gitpackage/Git.java
@@ -4,5 +4,6 @@ public class Git {
 	public static void main(String[] args) {
 		System.out.println("Git Project");
 		System.out.println("update Project");
+		System.out.println("update 2.0");
 	}
 }
